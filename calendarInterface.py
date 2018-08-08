3a7474bcd3b3b182c76d0d04c4d8654597c42921
Benjamin Weller <benjamin.weller@ibm.com>
2018-08-08 14:20:25 -0500

    I think I'm going to use my default arguments in this way for now. But I might add it using dictionary.get later.

--- calendarInterface.py
+++ calendarInterface.py
@@ -27,28 +27,60 @@ def main():
     return service
 
 
-def read(service_handler, date=datetime.datetime.utcnow().isoformat(), number_limit=10):
+def read(service_handler, now=None, number_limit=None):
+    if now == None:
+        now = datetime.datetime.utcnow()
+    if number_limit == None:
+        number_limit = 10
+
     # Call the Calendar API
-    now = date + 'Z'  # 'Z' indicates UTC time
-    # print('Getting the upcoming 10 events')
     events_result = service_handler.events().list(calendarId='primary', timeMin=now,
                                                   maxResults=number_limit, singleEvents=True,
                                                   orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
-        print('No upcoming events found.')
+        pyperclip.copy('No upcoming events found.')
     for event in events:
-        # noinspection PyInterpreter
         start = event['start'].get('dateTime', event['start'].get('date'))
+        # pyperclip.copy(pyperclip.paste()+f"\n{start} {event["summary"]}")
         # print(start, event['summary'])
 
+
 def write(service_handler, maya_time, reminder_level=1):
-    #I think that maya should handle both the date and the time????
+
+    event = {
+        'summary': 'Data Structures Assignment - 1',
+        'description': 'Theory Assessment',
+        'start': {
+            'dateTime': '2018-03-28T09:17:30+05:30',
+            'timeZone': 'Asia/Kolkata',  # my timezone
+        },
+        'end': {
+            'dateTime': '2018-03-28T17:18:30+05:30',  # 12 midnight is the deadline
+            'timeZone': 'Asia/Kolkata',
+        },
+        'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=1'
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},  # a day before start
+                {'method': 'popup', 'minutes': 100},  # 100 minutes before start
+            ],
+        },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    # print 'Event created: %s' % (event.get('htmlLink'))
+
+    # I think that maya should handle both the date and the time????
+
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
-
+    print(arguments)
     service_handler = main()
 
     if arguments["write"]:
@@ -56,8 +88,4 @@ if __name__ == '__main__':
     else:
         # Must be a read
         # Figure out how to do the optional params???
-        read(service_handler, arguments[], arguments[])
-
-
-
-
+        read(service_handler, arguments["date"], arguments["number_limit"])
